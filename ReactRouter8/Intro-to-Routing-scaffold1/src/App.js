1cfe77b6bde413241da72a4b6e7adf02ff8d3dd3
Suraj sahani <116185940+SurajSahani0011@users.noreply.github.com>
2024-05-19 19:37:21 +0530

    contextApi lec running

---
+++ ReactRouter8/Intro-to-Routing-scaffold1/src/App.js
@@ -0,0 +1,17 @@
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Items from './pages/Items'
+function App() {
+
+  const router = createBrowserRouter([
+    {path: "/" , element: <Home/>},
+    {path: "/about", element: <About/>},
+    {path: "items", element: <Items/>}
+  ])
+  return <>
+    <RouterProvider router={router}/>
+  </>
+}
+
+export default App;
